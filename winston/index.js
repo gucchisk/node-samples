ff78ea94f6ca2ddb6ec1e4f948bc38def6917727
gucchisk <gucchi_sk@yahoo.co.jp>
2021-11-14 17:18:31 +0900

    tweak

--- winston/index.js
+++ winston/index.js
@@ -1,38 +1,83 @@
-const { createLogger, format, transports } = require('winston')
+const { createLogger, format, transports, stream, Logger } = require('winston')
 const Transport = require('winston-transport')
 const { combine, timestamp, label, prettyPrint } = format
+const fs = require('fs')
 
 class CustomTransport extends Transport {
+  constructor() {
+    super()
+    // this.on('logged', (info) => {
+    //   console.log('event logged')
+    //   console.log(info)
+    // })
+  }
   log(info, callback) {
     setImmediate(() => {
-      this.emit('logged', info)
+      console.log(this.eventNames())
+      const result = this.emit('logged', info)
+      if (result) {
+        console.log('success')
+      }
     })
-    console.log('custom transport:')
+    // console.log('custom transport:')
     console.log(info)
+    console.log(callback)
     if (callback) {
       callback()
     }
   }
 }
 
+class MyConsole extends transports.Console {
+  constructor(opts = {}) {
+    super(opts)
+  }
+}
+
 const customFormat = format((info, opts = {}) => {
   console.log('custom format:')
   console.log(info)
-  return info
+  console.log(opts)
+  return false
 })
 
+const customTransport = new CustomTransport()
+
+const Writable = require('stream').Writable;
+
+class MyWritable extends Writable {
+  constructor(options) {
+    super(options);
+  }
+
+  _write(chunk, encoding, callback) {
+    console.log(chunk);
+    callback()
+  }
+}
+const st = new MyWritable()
 const logger = createLogger({
-  // level: 'debug',
-  format: combine(
-    label({ label: 'label' }),
-    timestamp(),
-    // prettyPrint()
-    customFormat()
-  ),
+  level: 'debug',
+  // format: combine(
+  //   label({ label: 'label' }),
+  // timestamp(),
+  // prettyPrint()
+  // customFormat()
+  // ),
   transports: [
-    // new transports.Console(),
-    new CustomTransport()
+    // new transports.Console({format: customFormat()}),
+    // new MyConsole(),
+    // customTransport,
+    new transports.Console(),
+    new transports.Stream({
+      // stream: fs.createWriteStream('/dev/null')
+      stream: st
+    })
   ],
+})
+
+logger.stream().on('data', (log) => {
+  console.log('log')
 })
 
 logger.debug('hello')
